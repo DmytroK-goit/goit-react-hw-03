41e0f2f411ce03baa85e908ee4285197cfe2ad64
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-09-12 09:20:14 +0300

    1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,17 @@
+import s from "./Contact.module.css";
 import { IoPeopleSharp } from "react-icons/io5";
 import { FaPhoneAlt } from "react-icons/fa";
 const Contact = ({ data, onDelete }) => {
   return (
-    <div key={data.id}>
-      <p>
-        <IoPeopleSharp /> {data.name}
-      </p>
-      <p>
-        <FaPhoneAlt /> {data.number}
-      </p>
+    <div className={s.contact} key={data.id}>
+      <div>
+        <p>
+          <IoPeopleSharp /> {data.name}
+        </p>
+        <p>
+          <FaPhoneAlt /> {data.number}
+        </p>
+      </div>
       <button type="button" onClick={() => onDelete(data.id)}>
         Dalete
       </button>
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,11 +1,12 @@
 import { useId } from "react";
+import s from "./SearchBox.module.css";
 
 const SearchBox = ({ value, onFilter }) => {
   const surchId = useId();
 
   return (
     <div>
-      <form>
+      <form className={s.form}>
         <label htmlFor={surchId}>
           Find contacts by name
           <input
